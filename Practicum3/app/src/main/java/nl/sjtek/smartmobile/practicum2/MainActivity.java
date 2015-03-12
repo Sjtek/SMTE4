5b54e17a9713362541e484c838db089ba8d9e89a
Wouter Habets <wouterhabets@gmail.com>
2015-03-12 10:41:25 +0100

    Added Practicum3 base

---
+++ Practicum3/app/src/main/java/nl/sjtek/smartmobile/practicum2/MainActivity.java
@@ -0,0 +1,25 @@
+package nl.sjtek.smartmobile.practicum2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickForward(View v) {
+        Intent intentReport = new Intent(this, ActivityReport.class);
+        startActivity(intentReport);
+    }
+}
