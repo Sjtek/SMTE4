dda0ac50b389b73b5fccb74d692607b7f7c3a089
I304248 <tijn.renders@student.fontys.nl>
2015-04-22 01:34:42 +0200

    Added Presentation and copied base for Practicum4

---
+++ Practicum4/app/src/main/java/nl/sjtek/smartmobile/practicum2/Criminal.java
@@ -0,0 +1,17 @@
+package nl.sjtek.smartmobile.practicum2;
+
+/**
+ * Created by wouter on 18-3-15.
+ */
+public class Criminal {
+
+    private String name;
+
+    public Criminal(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
